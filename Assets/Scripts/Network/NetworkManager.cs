64cdff4ce37738a0c2a1b20a1b08d3b2609482ce
jzy <359135103@qq.com>
2023-08-11 16:52:07 +0800

    kcp init

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Network
+{
+    /// <summary>
+    /// 网络管理
+    /// </summary>
+    public class NetworkManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # UGK-Client
 
 多人联网游戏demo，UGK-Server：unity、go、kcp server 。开发中......
+
+
+
+### TODO
+* 优先使用GalacticKittens 进行改造
+* Websocket支持添加 
+* 时间插值、客户端服务器时间同步
