db6a3138b08d8df7f35febe0e04ed9e40adee5a3
jzy <359135103@qq.com>
2023-09-07 19:01:01 +0800

    注释

--- Assets/Scripts/Network/Handlers/LoginHandler.cs
+++ Assets/Scripts/Network/Handlers/LoginHandler.cs
@@ -11,6 +11,11 @@ namespace Network.Handlers
     /// </summary>
     internal class LoginHandler
     {
+        /// <summary>
+        /// 心跳
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <param name="data"></param>
         [MessageMap(MID.HeartRes)]
         private static void Heart(Int64 timeStamp, byte[] data)
         {
@@ -19,7 +24,11 @@ namespace Network.Handlers
         }
         
         
-        
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <param name="data"></param>
         [MessageMap(MID.LoginRes)]
         private static void Login(Int64 timeStamp, byte[] data)
         {
@@ -29,6 +38,11 @@ namespace Network.Handlers
             MessageEventManager.Singleton.OnEvent(MessageEvent.Login,response);
         }
 
+        /// <summary>
+        /// 加载玩家数据
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <param name="data"></param>
         [MessageMap(MID.LoadPlayerRes)]
         private static void LoadPlayer(Int64 timeStamp, byte[] data)
         {
