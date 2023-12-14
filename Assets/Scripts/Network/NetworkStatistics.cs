46afd514cce8ed3db05238532805936835ad8abe
jzy <359135103@qq.com>
2023-12-14 11:53:25 +0800

    网络统计

---
+++ Assets/Scripts/Network/NetworkStatistics.cs
@@ -0,0 +1,129 @@
+using System;
+using Common.Tools;
+using Network.Sync;
+using UnityEngine;
+
+namespace Network
+{
+    /// <summary>
+    /// Shows Network messages and bytes sent and received per second.
+    /// </summary>
+    /// <remarks>
+    /// <para>Add this component to the same object as Network Manager.</para>
+    /// </remarks>
+    [DisallowMultipleComponent]
+    public class NetworkStatistics : MonoBehaviour
+    {
+        // update interval
+        double intervalStartTime;
+
+        // ---------------------------------------------------------------------
+
+        // CLIENT (public fields for other components to grab statistics)
+        // long bytes to support >2GB
+        [HideInInspector] public int  clientIntervalReceivedPackets;
+        [HideInInspector] public long clientIntervalReceivedBytes;
+        [HideInInspector] public int  clientIntervalSentPackets;
+        [HideInInspector] public long clientIntervalSentBytes;
+
+        // results from last interval
+        // long bytes to support >2GB
+        [HideInInspector] public int  clientReceivedPacketsPerSecond;
+        [HideInInspector] public long clientReceivedBytesPerSecond;
+        [HideInInspector] public int  clientSentPacketsPerSecond;
+        [HideInInspector] public long clientSentBytesPerSecond;
+
+
+        void Start()
+        {
+            // find available transport
+            Transport transport = Transport.active;
+            if (transport != null)
+            {
+                transport.OnClientDataReceived += OnClientReceive;
+                transport.OnClientDataSent += OnClientSend;
+            }
+            else Debug.LogError($"NetworkStatistics: no available or active Transport found on this platform: {Application.platform}");
+        }
+
+        void OnDestroy()
+        {
+            // remove transport hooks
+            Transport transport = Transport.active;
+            if (transport != null)
+            {
+                transport.OnClientDataReceived -= OnClientReceive;
+                transport.OnClientDataSent -= OnClientSend;
+            }
+        }
+
+        void OnClientReceive(ArraySegment<byte> data)
+        {
+            ++clientIntervalReceivedPackets;
+            clientIntervalReceivedBytes += data.Count;
+        }
+
+        void OnClientSend(ArraySegment<byte> data)
+        {
+            ++clientIntervalSentPackets;
+            clientIntervalSentBytes += data.Count;
+        }
+
+
+        void Update()
+        {
+            // calculate results every second
+            if (NetworkTime.LocalTime >= intervalStartTime + 1)
+            {
+                if (NetworkClient.active) UpdateClient();
+
+                intervalStartTime = NetworkTime.LocalTime;
+            }
+        }
+
+        void UpdateClient()
+        {
+            clientReceivedPacketsPerSecond = clientIntervalReceivedPackets;
+            clientReceivedBytesPerSecond = clientIntervalReceivedBytes;
+            clientSentPacketsPerSecond = clientIntervalSentPackets;
+            clientSentBytesPerSecond = clientIntervalSentBytes;
+
+            clientIntervalReceivedPackets = 0;
+            clientIntervalReceivedBytes = 0;
+            clientIntervalSentPackets = 0;
+            clientIntervalSentBytes = 0;
+        }
+
+        void OnGUI()
+        {
+            if (NetworkClient.active )
+            {
+                // create main GUI area
+                // 120 is below NetworkManager HUD in all cases.
+                GUILayout.BeginArea(new Rect(10, 120, 215, 300));
+
+                // show client / server stats if active
+                if (NetworkClient.active) OnClientGUI();
+
+                // end of GUI area
+                GUILayout.EndArea();
+            }
+        }
+
+        void OnClientGUI()
+        {
+            // background
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("<b>Client Statistics</b>");
+
+            // sending ("msgs" instead of "packets" to fit larger numbers)
+            GUILayout.Label($"Send: {clientSentPacketsPerSecond} msgs @ {Utils.PrettyBytes(clientSentBytesPerSecond)}/s");
+
+            // receiving ("msgs" instead of "packets" to fit larger numbers)
+            GUILayout.Label($"Recv: {clientReceivedPacketsPerSecond} msgs @ {Utils.PrettyBytes(clientReceivedBytesPerSecond)}/s");
+
+            // end background
+            GUILayout.EndVertical();
+        }
+    }
+}
